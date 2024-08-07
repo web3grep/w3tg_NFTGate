88f943e780f648687e5d8be154c9c031a613eff7
web3grep <122474751+web3grep@users.noreply.github.com>
2024-08-07 08:32:31 -0500

    Create config.py

---
+++ config.py
@@ -0,0 +1,12 @@
+# config.py
+
+
+BOT_TOKEN = '000000000:aaaaaaaaaa_aaaaaaaaaaaaaaaaaaaaaaaaaaaa' #token from BOTFATHRER
+DICTIONARY_FILE = 'user_addresses.json'
+USER_STATUS_FILE = 'user_statuses.json'
+LOGGING_LEVEL = 'DEBUG'
+GROUP_ID = -10000000000  # Замените на реальный ID вашей группы
+TOKENS_TO_CHECK = {"Base:0x49da377fa479f7c73b8512831e66e43d3f1b5ada:1"} #чейн, контракт, id
+CHECK_INTERVAL = 3600
+CONFIRMATION_CYCLES = 1 #циклов до смены статуса
+ANKR_API_KEY = "0000000000000000000000000000000000000000000000000000000000000" #Ankr api
